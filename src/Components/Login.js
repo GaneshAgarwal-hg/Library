8d672d189aaf4f24752203488418bab7d175e261
GaneshAgarwal-hg <73985223+GaneshAgarwal-hg@users.noreply.github.com>
2022-11-22 11:56:20 +0530

    Add files via upload

---
+++ src/Components/Login.js
@@ -0,0 +1,106 @@
+import React, { useState, useRef } from "react";
+import { useNavigate } from 'react-router-dom';
+import AuthService from "../Services/auth-service";
+import library from "../library.jpg";
+import './login.css'
+const Login = () => {
+  let navigate = useNavigate();
+
+  // const form = useRef();
+  // const checkBtn = useRef();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const onChangeUsername = (e) => {
+    const username = e.target.value;
+    setUsername(username);
+  };
+
+  const onChangePassword = (e) => {
+    const password = e.target.value;
+    setPassword(password);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+      AuthService.login(username, password).then(
+        () => {
+          navigate("/Displaybookuser");
+          window.location.reload();
+        },
+        (error) => {
+          const resMessage =
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString();
+        }
+      );
+  };
+
+  return (
+    <>
+    <section class="vh-100">
+  <div class="container-fluid h-custom">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-md-9 col-lg-6 col-xl-5">
+        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+          class="img-fluid" alt="Sample image"/>
+      </div>
+      <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+      <form onSubmit={handleLogin}>
+      <div className="card card-container">
+      <img
+          src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+          alt="profile-img"
+          className="profile-img-card"
+        />
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              className="form-control"
+              name="username"
+              value={username}
+              onChange={onChangeUsername}
+              
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              value={password}
+              onChange={onChangePassword}
+                          />
+          </div>
+
+          <div className="form-group" style={{"marginTop":"10px"}}>
+            <button className="btn btn-primary btn-block">
+              <span>Login</span>
+            </button>
+          </div>
+          </div>
+          
+        </form>
+      </div>
+    </div>
+  </div>
+  <div
+    class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
+    <div class="text-white mb-3 mb-md-0">
+      Copyright © 2022. All rights reserved.
+    </div>
+ 
+  </div>
+</section>
+    </>
+  );
+};
+
+export default Login;
